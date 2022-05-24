4acd0ae52a6a554a13ea3d6895054a99d259583f
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2022-05-24 11:01:49 -0500

    Update kmp.cpp

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -4,26 +4,28 @@
 using namespace std;
 typedef long long ll;
 int main(){
-    string p;cin>>p;
-    int i = 0;
-    int n = p.size();
-    vector<int> phi(n);
-    phi[0] = 0;
-    for(int j = 1 ;j < n; j++){
-      while(i > 0 && p[i]!=p[j]) i = phi[i-1];
-      if(p[i] == p[j]) i++;
-      phi[j] = i;
-    }
-    string t;cin>>t;
-    int match = 0;
-    for(int j=0, i=0;j<t.size();j++){
-      while(i>0 && p[i]!=t[j]) i = phi[i-1];
-      if(p[i] == t[j])i++;
-      if(i==p.size()){
-        match++;
-        i = phi[i-1];
+    string pattern;cin>>pattern;
+    string txt;cin>>txt;
+    function<int(string,string)> kmp = [&](string p,string t){
+      int i = 0;
+      int n = p.size();
+      vector<int> phi(n);
+      phi[0] = 0;
+      for(int j = 1 ;j < n; j++){
+        while(i > 0 && p[i]!=p[j]) i = phi[i-1];
+        if(p[i] == p[j]) i++;
+        phi[j] = i;
       }
-    }
-    cout<<match<<endl;
+      int match = 0;
+      for(int j=0, i=0;j<t.size();j++){
+        while(i>0 && p[i]!=t[j]) i = phi[i-1];
+        if(p[i] == t[j])i++;
+        if(i==p.size()){
+          match++;
+          i = phi[i-1];
+        }
+      }
+      return match;
+    };
+    cout<<kmp(pattern,txt)<<endl;
 }
-
